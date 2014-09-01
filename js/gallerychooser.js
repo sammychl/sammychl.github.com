83daa5dcca7df5335ba08e4877699c44b4274e05
sammychl <sammychl@gmail.com>
2014-09-01 06:15:52 +0000

    meta data

--- js/gallerychooser.js
+++ js/gallerychooser.js
@@ -5,32 +5,32 @@
 var imageFiles = [];
 var options = {
 
-    // Required. Called when a user selects an item in the Chooser.
-    success: function(files) {
-	imageFiles = files;
-	alert("Here's the file link: " + files[0].link);
-    },
+	// Required. Called when a user selects an item in the Chooser.
+	success: function(files) {
+		imageFiles = files;
+		alert("Here's the file link: " + files[0].link);
+	},
 
-    // Optional. Called when the user closes the dialog without selecting a file
-    // and does not include any parameters.
-    cancel: function() {
-	alert("user cancelled by closing dialog without selecting file");
-    },
+	// Optional. Called when the user closes the dialog without selecting a file
+	// and does not include any parameters.
+	cancel: function() {
+		alert("user cancelled by closing dialog without selecting file");
+	},
 
-    // Optional. "preview" (default) is a preview link to the document for sharing,
-    // "direct" is an expiring link to download the contents of the file. For more
-    // information about link types, see Link types below.
-    linkType: "direct", // or "direct"
+	// Optional. "preview" (default) is a preview link to the document for sharing,
+	// "direct" is an expiring link to download the contents of the file. For more
+	// information about link types, see Link types below.
+	linkType: "direct", // or "direct"
 
-    // Optional. A value of false (default) limits selection to a single file, while
-    // true enables multiple file selection.
-    multiselect: true,
+	// Optional. A value of false (default) limits selection to a single file, while
+	// true enables multiple file selection.
+	multiselect: true,
 
-    // Optional. This is a list of file extensions. If specified, the user will
-    // only be able to select files with these extensions. You may also specify
-    // file types, such as "video" or "images" in the list. For more information,
-    // see File types below. By default, all extensions are allowed.
-    extensions: ['.jpg']
+	// Optional. This is a list of file extensions. If specified, the user will
+	// only be able to select files with these extensions. You may also specify
+	// file types, such as "video" or "images" in the list. For more information,
+	// see File types below. By default, all extensions are allowed.
+	extensions: ['.jpg']
 };
 
 var button = Dropbox.createChooseButton(options);
@@ -43,32 +43,34 @@ document.getElementById("dropboxChooser").appendChild(button);
 var app = angular.module('galleryChooser', ['firebase']);
 
 app.controller('galleryChooserCtrl', function($scope, $firebase) {
-    console.log('hello');
-    var galleryRef = new Firebase('https://glowing-fire-6466.firebaseIO.com/gallery');
-    var galleryRefSync = $firebase(galleryRef);
-    
-    var galleriesArr = galleryRefSync.$asArray();
+	console.log('hello');
+	var galleryRef = new Firebase('https://glowing-fire-6466.firebaseIO.com/gallery');
+	var galleryRefSync = $firebase(galleryRef);
+	
+	var galleriesArr = galleryRefSync.$asArray();
 
-    $scope.misc = {test:"testing"};
+	$scope.misc = {test:"testing"};
 
 	$scope.photos = [];
-
-    $scope.firebasePush = function() {
-	console.log('firebasePush');
-	galleriesArr.$add({
-	    weddingDate: new Date(),
-	    tags: ['wedding', 'vancouver', 'engagement'],
-	    title: 'Tyler and Erin',
-	    files: imageFiles
-	}).then(function(ref) {
-		var id = ref.name();
-		console.log("added record with id " + id);
-		$scope.photos = galleriesArr[galleriesArr.$indexFor(id)].files;
-	});
+	$scope.meta = {};
+	$scope.firebasePush = function() {
+		console.log('firebasePush');
+		galleriesArr.$add({
+			weddingDate: new Date(),
+			tags: $scope.meta.tags,
+			title: $scope.meta.title,
+			password: $scope.meta.password,
+			files: imageFiles
+		}).then(function(ref) {
+			var id = ref.name();
+			console.log("added record with id " + id);
+			$scope.photos = galleriesArr[galleriesArr.$indexFor(id)].files;
+			$scope.post = galleriesArr[galleriesArr.$indexFor(id)];
+		});
+		
+	};
 	
-    };
-    
-    
+	
 });
 
 
