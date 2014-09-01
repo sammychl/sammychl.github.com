5e889dd64ae30cde49c2dac72ea6ed123ff6f794
sammychl <sammychl@gmail.com>
2014-09-01 00:21:48 +0000

    button to push firebase

--- js/gallerychooser.js
+++ js/gallerychooser.js
@@ -1,37 +1,37 @@
 /*******************************
-** stuff for dropbox chooser api**
-******************************/
+ ** stuff for dropbox chooser api**
+ ******************************/
 
 var imageFiles = [];
 
 var options = {
 
-    // Required. Called when a user selects an item in the Chooser.
-    success: function(files) {
-	    imageFiles = files;
-	    alert("Here's the file link: " + files[0].link);
-    },
+	// Required. Called when a user selects an item in the Chooser.
+	success: function(files) {
+		imageFiles = files;
+		alert("Here's the file link: " + files[0].link);
+	},
 
-    // Optional. Called when the user closes the dialog without selecting a file
-    // and does not include any parameters.
-    cancel: function() {
-	    alert("user cancelled by closing dialog without selecting file");
-    },
+	// Optional. Called when the user closes the dialog without selecting a file
+	// and does not include any parameters.
+	cancel: function() {
+		alert("user cancelled by closing dialog without selecting file");
+	},
 
-    // Optional. "preview" (default) is a preview link to the document for sharing,
-    // "direct" is an expiring link to download the contents of the file. For more
-    // information about link types, see Link types below.
-    linkType: "direct", // or "direct"
+	// Optional. "preview" (default) is a preview link to the document for sharing,
+	// "direct" is an expiring link to download the contents of the file. For more
+	// information about link types, see Link types below.
+	linkType: "direct", // or "direct"
 
-    // Optional. A value of false (default) limits selection to a single file, while
-    // true enables multiple file selection.
-    multiselect: true,
+	// Optional. A value of false (default) limits selection to a single file, while
+	// true enables multiple file selection.
+	multiselect: true,
 
-    // Optional. This is a list of file extensions. If specified, the user will
-    // only be able to select files with these extensions. You may also specify
-    // file types, such as "video" or "images" in the list. For more information,
-    // see File types below. By default, all extensions are allowed.
-    extensions: ['.jpg']
+	// Optional. This is a list of file extensions. If specified, the user will
+	// only be able to select files with these extensions. You may also specify
+	// file types, such as "video" or "images" in the list. For more information,
+	// see File types below. By default, all extensions are allowed.
+	extensions: ['.jpg']
 };
 
 var button = Dropbox.createChooseButton(options);
@@ -39,14 +39,17 @@ document.getElementById("dropboxChooser").appendChild(button);
 
 
 /********************************
-** stuff for firebase uploading **
-********************************/
+ ** stuff for firebase uploading **
+ ********************************/
 
 var galleryRef = new Firebase('https://glowing-fire-6466.firebaseIO.com/gallery');
 
-galleryRef.push({
-	weddingDate: new Date(),
-	tags: ['wedding', 'vancouver', 'engagement'],
-	title: 'Tyler and Erin',
-	files: imageFiles
-});
+var firebaseAdd = function() {
+	galleryRef.push({
+		weddingDate: new Date(),
+		tags: ['wedding', 'vancouver', 'engagement'],
+		title: 'Tyler and Erin',
+		files: imageFiles
+	});
+	
+};
