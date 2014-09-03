4ce751b89e8f920bd54cdb7bb0de21a9dd41fbd5
sammy li <sammychl@hotmail.com>
2014-09-03 00:47:48 +0000

    passwordError message

--- js/gallery.js
+++ js/gallery.js
@@ -15,30 +15,34 @@ app.controller('galleryCtrl', function($scope, $firebase) {
 	$scope.posts = galleriesArr;
 	console.log(galleriesArr);
 	$scope.security = {password:"", passwordValid:false};
-	$scope.selectedPost = {};
+	$scope.selectedPost = false;
 	$scope.setSelectedPost = function(id) {
 		post = galleriesArr.$getRecord(id);
 		console.log($scope.selectedPost);
-		if (post.password === $scope.security.password || post.password == "") {
+		if (post.password === $scope.security.password || post.password === "") {
 			$scope.security.passwordValid = true;
+			$scope.security.passwordError = "";
 			$scope.selectedPost = post;
 		}
 		else {
 			$scope.security.passwordValid = false;
+			$scope.security.passwordError = "Please enter Password";
 			$scope.selectedPost = {};
 		}
 		console.log($scope.selectedPost);
 	};
 
 	$scope.checkPassword = function() {
-		if (post.password === $scope.security.password || post.password == "") {
+		if (post.password === $scope.security.password || post.password === "") {
 			$scope.security.passwordValid = true;
+			$scope.security.passwordError = "";
 			$scope.selectedPost = post;
 		}
 		else {
 			$scope.security.passwordValid = false;
+			$scope.security.passwordError = "Incorrect Password";
 			$scope.selectedPost = {};
 		}
 
-	}
+	};
 });
