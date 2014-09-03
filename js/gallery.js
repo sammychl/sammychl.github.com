eb7a2ed8a4326550e77ccfb9dac5dd2d2328cb92
sammy li <sammychl@hotmail.com>
2014-09-03 00:24:26 +0000

    password entry

--- js/gallery.js
+++ js/gallery.js
@@ -8,20 +8,37 @@ app.controller('galleryCtrl', function($scope, $firebase) {
 	var galleryRef = new Firebase('https://glowing-fire-6466.firebaseIO.com/gallery');
 	var galleryRefSync = $firebase(galleryRef);
 	var galleriesArr = galleryRefSync.$asArray();
-
+	var post;
 
 	$scope.photos = [];
 	$scope.meta = {};
 	$scope.posts = galleriesArr;
 	console.log(galleriesArr);
-	$scope.security = {password:""};
+	$scope.security = {password:"", passwordValid:false};
 	$scope.selectedPost = {};
 	$scope.setSelectedPost = function(id) {
-		var post = galleriesArr.$getRecord(id);
+		post = galleriesArr.$getRecord(id);
 		console.log($scope.selectedPost);
-		if (post.password === $scope.security.password) {
-			$scope.selectedPost = post;	
+		if (post.password === $scope.security.password || post.password == "") {
+			$scope.security.passwordValid = true;
+			$scope.selectedPost = post;
+		}
+		else {
+			$scope.security.passwordValid = false;
+			$scope.selectedPost = {};
 		}
 		console.log($scope.selectedPost);
-	}; 
+	};
+
+	$scope.checkPassword = function() {
+		if (post.password === $scope.security.password || post.password == "") {
+			$scope.security.passwordValid = true;
+			$scope.selectedPost = post;
+		}
+		else {
+			$scope.security.passwordValid = false;
+			$scope.selectedPost = {};
+		}
+
+	}
 });
