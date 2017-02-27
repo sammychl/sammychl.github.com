71a1ec39461e569e93f2e56ba3c85022e7e4edc8
sam <sammychl@hotmail.com>
2017-02-27 11:11:02 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -23,6 +23,7 @@
 
 	
 	//nivo lightbox
+	/* comment out */
 	$('.gallery-item a').nivoLightbox({
 		effect: 'fadeScale',                             // The effect to use when showing the lightbox
 		theme: 'default',                           // The lightbox theme to use
@@ -37,6 +38,6 @@
 		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
 		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
 	});
-
+	/*commented out*/
 	
 })(jQuery);
